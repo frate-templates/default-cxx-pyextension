215e8b2606d6bbcd5628a2e0b6f12dc939d8bbba
lucas sproule <lucas.sproule.42@gmail.com>
2024-02-12 14:26:45 -0500

    fix build commmand

--- __init__/build_command.lua
+++ __init__/build_command.lua
@@ -1,9 +1,14 @@
-project.set_build_command(frate.format([[
-cmake -DCMAKE_BUILD_TYPE={%current_mode%}  
-make -j {%jobs%} ./{%build_dir%}/{%name%}
+
+set_build_command(plocal,frate.format([[
+cd {%path%};
+cmake -DCMAKE_BUILD_TYPE={%build_mode%};
+make -j {%jobs%}
+python {%build_dir%}/main.py
 ]], {
     current_mode = project.current_mode,
     build_dir = project.build_dir,
+    path = get_project_path(plocal),
     name = project.name,
-    jobs = plocal.get_requested_jobs()
+    jobs = get_requested_jobs(plocal),
+    build_mode = get_build_mode(plocal)
 }))
